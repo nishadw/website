1103fb9d56e0e414a37a0dfcdcc999ad85f04931
Nishad Wajge <nishad.wajge@gmail.com>
2026-04-20 00:19:52 -0400

    stocks v1

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/stocks/:path*',
+        destination: 'https://13f-analyzer-6fp3kyd0o-nishadws-projects.vercel.app/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
